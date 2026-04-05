4e6a40fda31f29ec85b6350fbf75c2238007c595
roshanj522025 <roshanj522025@gmail.com>
2026-04-06 03:22:36 +0530

    Create setup.sh

---
+++ .devcontainer/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Initialize pacman keys (required for Arch containers)
+pacman-key --init
+pacman-key --populate archlinux
+pacman -Syu --noconfirm
+
+# Install i3, VNC, and essentials
+pacman -S --noconfirm \
+    i3-wm \
+    i3status \
+    dmenu \
+    tigervnc \
+    xorg-server \
+    xorg-xinit \
+    xorg-xsetroot \
+    xorg-xrandr \
+    xterm \
+    alacritty \
+    ttf-dejavu \
+    ttf-liberation \
+    firefox \
+    novnc \
+    python-websockify \
+    net-tools
+
+# Create VNC config directory
+mkdir -p ~/.vnc
+
+# Create xstartup script (launches i3 when VNC connects)
+cat > ~/.vnc/xstartup << 'EOF'
+#!/bin/sh
+unset SESSION_MANAGER
+unset DBUS_SESSION_BUS_ADDRESS
+
+# Set background color
+xsetroot -solid "#2E3436"
+
+# Start i3
+exec i3
+EOF
+
+chmod +x ~/.vnc/xstartup
+
+# Set default VNC password (change this!)
+echo "codespaces" | vncpasswd -f > ~/.vnc/passwd
+chmod 600 ~/.vnc/passwd
+
+echo "Setup complete. Run 'vncserver :1' to start."
